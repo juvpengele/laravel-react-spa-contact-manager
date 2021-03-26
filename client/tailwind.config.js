f74c6afbb54c5863f63a3ed8d4eb05ccf95e264c
Juvenal Pengele <juvenal.pengele@gmail.com>
2021-03-26 18:11:13 +0000

    Client feat: Add tailwind css config files

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['"DM Sans"', ...defaultTheme.fontFamily.sans]
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
