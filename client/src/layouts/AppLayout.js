37dfc390a9720093a0abfe40d3743de8e3802d93
Juvenal Pengele <juvenal.pengele@gmail.com>
2021-06-07 16:44:23 +0000

    Integrate navbar in the auth layout

--- client/src/layouts/AppLayout.js
+++ client/src/layouts/AppLayout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/layouts/Navbar";
+import Sidebar from "../components/layouts/Sidebar";
 
 function AppLayout({ children }) {
     return (
-        <div>
-            <Navbar/>
-            <div className="bg-blue-50 min-h-screen">
+        <div className="flex">
+            <Sidebar className="w-1/5"/>
+            <div className="bg-blue-50 min-h-screen w-4/5">
                 { children }
             </div>
-
         </div>
     )
 }
